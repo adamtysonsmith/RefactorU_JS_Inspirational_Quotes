db8971395742626c50dc77e27fff52c16f0dc16a
Adam Smith <atsmith227@gmail.com>
2015-07-08 14:14:18 -0600

    Interface completed

--- quotes.js
+++ quotes.js
@@ -9,3 +9,17 @@ function supports_local_storage() {
 console.log('Supports LocalStorage?:',supports_local_storage());
 
 
+////////////////////////////////////
+// Storage Strategy
+////////////////////////////////////
+
+// Since localStorage can only store strings:
+// We will store a json object in our data var, then serialize into localStorage using json.stringify()
+// We deserialize using json.parse() to work with the data as a JavaScript object
+var storage = this.localStorage;
+var data;
+
+
+////////////////////////////////////
+// Storage Strategy
+////////////////////////////////////
